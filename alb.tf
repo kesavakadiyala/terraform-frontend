89249ac86728c1ccb62c150939428913c6e0cf09
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-03-04 15:59:10 +0530

    Removed ALB from frontend and added it in VPC

--- alb.tf
+++ alb.tf
@@ -1,17 +1,5 @@
-resource "aws_lb" "frontend-alb" {
-  name               = "frontend"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow-frontend-alb.id]
-  subnets            = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET
-  tags = {
-    Name        = var.component
-    Environment = var.ENV
-  }
-}
-
 resource "aws_lb_listener" "frontend-lb-listener" {
-  load_balancer_arn = aws_lb.frontend-alb.arn
+  load_balancer_arn = data.terraform_remote_state.vpc.outputs.FRONTEND_ALB_ARN
   port              = "80"
   protocol          = "HTTP"
   default_action {
@@ -20,20 +8,8 @@ resource "aws_lb_listener" "frontend-lb-listener" {
   }
 }
 
-resource "aws_lb" "backend-alb" {
-  name               = "backend-alb"
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow-backend-alb.id]
-  subnets            = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET
-  tags = {
-    Name        = var.component
-    Environment = var.ENV
-  }
-}
-
 resource "aws_lb_listener" "backend" {
-  load_balancer_arn = aws_lb.backend-alb.arn
+  load_balancer_arn = data.terraform_remote_state.vpc.outputs.BACKEND_ALB_ARN
   port              = "80"
   protocol          = "HTTP"
   default_action {
@@ -53,5 +29,5 @@ resource "aws_route53_record" "backend-alb" {
   type                = "CNAME"
   zone_id             = data.terraform_remote_state.vpc.outputs.HOSTED_ZONE_ID
   ttl                 = "300"
-  records             = [aws_lb.backend-alb.dns_name]
+  records             = [data.terraform_remote_state.vpc.outputs.BACKEND_ALB_DNSNAME]
 }
