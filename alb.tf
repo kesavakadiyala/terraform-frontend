f1bdbcb5644aead4521c09ccbdd781436b157b62
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-03-09 15:28:07 +0530

    Adding ALB's for Prod

--- alb.tf
+++ alb.tf
@@ -1,5 +1,6 @@
-resource "aws_lb_listener" "frontend-lb-listener" {
-  load_balancer_arn = data.terraform_remote_state.vpc.outputs.FRONTEND_ALB_ARN
+resource "aws_lb_listener" "frontend-lb-listener-dev" {
+  count = var.ENV == "dev" ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.vpc.outputs.FRONTEND_ALB_ARN_DEV
   port              = "80"
   protocol          = "HTTP"
   default_action {
@@ -8,8 +9,37 @@ resource "aws_lb_listener" "frontend-lb-listener" {
   }
 }
 
-resource "aws_lb_listener" "backend" {
-  load_balancer_arn = data.terraform_remote_state.vpc.outputs.BACKEND_ALB_ARN
+resource "aws_lb_listener" "backend-dev" {
+  count = var.ENV == "dev" ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.vpc.outputs.BACKEND_ALB_ARN_DEV
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "OK"
+      status_code  = "200"
+    }
+  }
+}
+
+
+resource "aws_lb_listener" "frontend-lb-listener-prod" {
+  count = var.ENV == "prod" ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.vpc.outputs.FRONTEND_ALB_ARN_PROD
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend-lb-target-group.arn
+  }
+}
+
+resource "aws_lb_listener" "backend-prod" {
+  count = var.ENV == "prod" ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.vpc.outputs.BACKEND_ALB_ARN_PROD
   port              = "80"
   protocol          = "HTTP"
   default_action {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "BACKEND_LISTENER_ARN" {
-  value = aws_lb_listener.backend.arn
+output "BACKEND_LISTENER_ARN_DEV" {
+  value = aws_lb_listener.backend-dev.arn
+}
+
+output "BACKEND_LISTENER_ARN_PROD" {
+  value = aws_lb_listener.backend-prod.arn
 }
