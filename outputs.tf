94bdb84b4c37b073305b33d09ebdce92b10e092a
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-03-10 08:15:17 +0530

    Bug Fixes

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "BACKEND_LISTENER_ARN_DEV" {
-  value = aws_lb_listener.backend-dev.arn
+  value = aws_lb_listener.backend-dev.*.arn
 }
 
 output "BACKEND_LISTENER_ARN_PROD" {
-  value = aws_lb_listener.backend-prod.arn
+  value = aws_lb_listener.backend-prod.*.arn
 }
